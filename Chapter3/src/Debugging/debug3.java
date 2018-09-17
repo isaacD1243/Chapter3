3d229ff4b9276b404250972eacdc2a7919ac4f33
Dibbern <ID129001@davenportschools.org>
2018-09-17 08:56:00 -0500

    finnished debugging

--- Chapter3/src/Debugging/debug3.java
+++ Chapter3/src/Debugging/debug3.java
@@ -4,7 +4,22 @@ public class debug3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 String name;
+	      name = getName)(;
+	      displayGreeting(name);           
+	   }
+	   public static String getName()
+	   {
+	      String name;
+	      Scanner input = new Scanner(System.in);
+	      System.out.print("Enter name ");
+	      name = input.nextLine();
+	      retrn name;
+	   }
+	   public static void displayGreeting(int name)
+	   {
+	      System.out.println("Hello, " + name + "!");
+	   }
 	}
 
 }
--- Chapter3/src/Debugging/debug4.java
+++ Chapter3/src/Debugging/debug4.java
@@ -4,7 +4,17 @@ public class debug4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 int myCredits = 13;
+	      int yourCredits = 17;
+	      double rate = 75.84;
+	      System.out.println("My tuition:");
+	      tuitionBill(myCredits, rate);
+	      System.out.println("Your tuition:");
+	      tuitionBill(yourCredits, myCredits);
+	   }
+	   public static void tuitionBill(int c double r)
+	   {
+	      System.out.println("Total due " + (r*c));
 	}
 
 }
